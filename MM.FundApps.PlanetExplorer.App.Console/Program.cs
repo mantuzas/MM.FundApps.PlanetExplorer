50d816dd9df1d53e3646c79eed8e51f984c89900
Mantas <mantuzas@gmail.com>
2017-05-08 23:05:32 +0100

    Adding initial project  with  readme markdown.

---
+++ MM.FundApps.PlanetExplorer.App.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MM.FundApps.PlanetExplorer.App.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {   
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+**Tools:**
+* Visual Studio 2017
+
+**Frameworks:**
+* .net core 1.1
+* xunit 2.2.0
+
+**Startup project:**
+* MM.FundApps.PlanetExplorer.App.Console

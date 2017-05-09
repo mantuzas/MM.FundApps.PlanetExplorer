ae38994ac34edec63f5c95bf1f6e3ab5cc67bb95
Mantas <mantuzas@gmail.com>
2017-05-09 22:52:35 +0100

    Adding MM.FundApps.PlanetExplorer.Robot.Abstractions.Tests to CI build.

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 
 dotnet restore
 
-dotnet test ./MM.FundApps.PlanetExplorer.Robot.Tests/MM.FundApps.PlanetExplorer.Robot.Tests.csproj -c Release
 dotnet test ./MM.FundApps.PlanetExplorer.RemoteControlCenter.Tests/MM.FundApps.PlanetExplorer.RemoteControlCenter.Tests.csproj -c Release
+dotnet test ./MM.FundApps.PlanetExplorer.Robot.Abstractions.Tests/MM.FundApps.PlanetExplorer.Robot.Abstractions.Tests.csproj -c Release
+dotnet test ./MM.FundApps.PlanetExplorer.Robot.Tests/MM.FundApps.PlanetExplorer.Robot.Tests.csproj -c Release
 
 dotnet build -c Release
 
